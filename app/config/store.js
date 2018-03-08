08f932d5529b63f7ebd83776d35417899e84514c
Dillon Nys <dnys1@asu.edu>
2018-03-08 09:24:55 -0700

    Added react-navigation to Redux

--- app/config/store.js
+++ app/config/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
+import { createReactNavigationReduxMiddleware } from 'react-navigation-redux-helpers';
 import Logger from 'redux-logger';
 
-import Reducers from '../reducers';
+import reducers from '../reducers';
 
 const middleware = [];
+
+// Must be run before createReduxBoundAddListener
+middleware.push(createReactNavigationReduxMiddleware('root', state => state.nav));
+
 if (process.env.NODE_ENV === 'development') {
   middleware.push(Logger);
 }
 
-export default createStore(Reducers, applyMiddleware(...middleware));
+export default createStore(reducers, applyMiddleware(...middleware));
