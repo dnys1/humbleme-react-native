387f24f4012baae0ea72ce78072271970a8ecb76
Dillon Nys <dnys1@asu.edu>
2018-03-09 09:07:26 -0700

    Begin saga implement

--- app/config/store.js
+++ app/config/store.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createReactNavigationReduxMiddleware } from 'react-navigation-redux-helpers';
 import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
 import thunk from 'redux-thunk';
 
 import reducers from '../reducers';
+import rootSaga from './sagas';
 
-const middleware = [];
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
 
 // Must be run before createReduxBoundAddListener
 middleware.push(createReactNavigationReduxMiddleware('root', state => state.nav));
@@ -15,4 +18,8 @@ if (process.env.NODE_ENV === 'development') {
   middleware.push(logger);
 }
 
-export default createStore(reducers, applyMiddleware(...middleware));
+const store = createStore(reducers, applyMiddleware(...middleware));
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
