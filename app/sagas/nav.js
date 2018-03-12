bde5d19c297ecd504800937de04d35683ec29c66
Dillon Nys <dnys1@asu.edu>
2018-03-12 15:25:29 -0700

    Fixed nav issue in WelcomeStack

--- app/sagas/nav.js
+++ app/sagas/nav.js
@@ -1,16 +1,15 @@
 import { Keyboard } from 'react-native';
-import { take, fork, put, race, all } from 'redux-saga/effects';
+import { take, fork, put, race, all, takeEvery } from 'redux-saga/effects';
 
 import { NAV_LOGGED_IN_SCREEN } from '../actions/nav';
 import { DISMISS_KEYBOARD } from '../actions/app';
 import { CLEAR_SIGNUP_DATA, CLEAR_LOGIN_DATA } from '../actions/welcome';
 
+// Take every back button press (takeEvery will never win), until user logins in
 function* watchBackNav() {
-  while (true) {
-    yield race([take('Navigation/BACK'), take(NAV_LOGGED_IN_SCREEN)]);
-    yield put({ type: CLEAR_SIGNUP_DATA });
-    yield put({ type: CLEAR_LOGIN_DATA });
-  }
+  yield race([takeEvery('Navigation/BACK', () => null), take(NAV_LOGGED_IN_SCREEN)]);
+  yield put({ type: CLEAR_SIGNUP_DATA });
+  yield put({ type: CLEAR_LOGIN_DATA });
 }
 
 function* watchNavTransition() {
